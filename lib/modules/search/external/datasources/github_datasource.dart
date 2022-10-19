52b1b2f1dca18eaa20d170c4040d79aebabc8f01
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-10-18 22:03:29 -0300

    Implements external

--- lib/modules/search/external/datasources/github_datasource.dart
+++ lib/modules/search/external/datasources/github_datasource.dart
@@ -2,14 +2,26 @@ import 'package:clean_architecture_app/modules/search/infra/datasources/search_d
 import 'package:clean_architecture_app/modules/search/infra/models/result_search_model.dart';
 import 'package:dio/dio.dart';
 
+extension on String{
+  normalizeText(){
+    return this.replaceAll(" ", "+");
+  }
+}
+
 class GithubDataSource implements SearchDataSource{
 
   final Dio dio;
   GithubDataSource(this.dio);
 
   @override
-  Future<List<ResultSearchModel>?> getSearch(String? searchText) {
-    dio.
+  Future<List<ResultSearchModel>?> getSearch(String? searchText) async{
+    final response = await dio.get("https://api.github.com/search/users?q=${searchText!.normalizeText()}");
+    print("Status code");
+    if(response.statusCode == 200) {
+      // final formatedReturn = response as List<ResultSearchModel>?;
+      final list = (response.data["items"] as List).map((e) => ResultSearchModel?.fromMap(e)).toList();
+      return list;
+    }
   }
   
 }
--- test/modules/search/external/datasources/github_datasource_test.dart
+++ test/modules/search/external/datasources/github_datasource_test.dart
@@ -1,22 +1,31 @@
+import 'dart:convert';
+
 import 'package:clean_architecture_app/modules/search/external/datasources/github_datasource.dart';
-import 'package:clean_architecture_app/modules/search/infra/datasources/search_datasources.dart';
-import 'package:clean_architecture_app/modules/search/infra/models/result_search_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class DioMock extends Mock implements Dio{}
+import '../../utils/github_result_search.dart';
 
-main(){
+class DioMock extends Mock implements Dio {
+  @override
+    Future<Response<T>> get<T>(
+    String? path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    void Function(int, int)? onReceiveProgress,
+  });
+}
 
+main() {
   final dio = DioMock();
   final datasource = GithubDataSource(dio);
 
   test("Deve retornar uma lista de ResultSearchModel", () async {
-
-    when(dio.get("")).thenAnswer((_) async => Response(data: {}, statusCode: 200, requestOptions: RequestOptions(path: "/")));
+    when(dio.get(any)).thenAnswer((_) async => Response(
+        data: jsonDecode(githubResult), statusCode: 200, requestOptions: RequestOptions(path: "")));
     final result = datasource.getSearch("searchText");
     expect(result, completes);
   });
-
 }
